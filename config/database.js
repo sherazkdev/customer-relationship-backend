621994e42f7e9a14d939ba1d7ba8c86efbea6e1d
Sheraz Dev <sherazk.dev@gmail.com>
2025-12-05 15:12:21 +0500

    Updated db name

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}/customerRelationShip`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     return conn;
   } catch (error) {
